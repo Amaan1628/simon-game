92a2bff123adffa0c09a503339fea1d5f89fc5ea
Amaan <118141092+Amaan1628@users.noreply.github.com>
2024-03-06 03:04:12 +0530

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
@@ -19,13 +18,42 @@ $(document).keydown( function() {
 $(".btn").click(function() {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+
     playSound(userChosenColour);
     animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length - 1);
 });
+
+
 
 //-------------------------------------------------------------------------------------------------
 
+function checkAnswer (currentLevel) {
+    if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+        console.log("success");
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout ( function () {
+                nextSequence(); }, 1000);
+        }
+    }
+    else {
+        console.log("wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout ( function () {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+}
+
 function nextSequence() {
+
+    userClickedPattern = [];
+
     level++;
     $("#level-title").text("Level " + level);
     
@@ -50,3 +78,8 @@ function animatePress(currentColour) {
     } , 100);
 }
 
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
